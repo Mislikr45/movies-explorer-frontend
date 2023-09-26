ceb5807ef405afd48346f65aaacba36778c06ee7
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-09-26 19:26:06 +0500

    1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -73,7 +73,7 @@ function App() {
       .then(([userProfile, moviesUser, movies]) => {
         setCurrentUser(userProfile);
         setMovies(movies);
-
+        console.log(userProfile, moviesUser, movies)
         setMoviesUser(moviesUser);
       })
       .catch((error) => console.log(`Ошибка: ${error}`));
