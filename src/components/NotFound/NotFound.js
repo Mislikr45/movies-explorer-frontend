2a3e8e0942bc1f44af85a7cc02e320dc72c396ee
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-09-30 17:29:17 +0500

    1

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,7 +5,8 @@ import { useNavigate } from "react-router-dom";
 function NotFound() {
 	const navigate = useNavigate();
 	const goBack = () => {
-		navigate(-2)
+		console.log('1')
+		navigate(-1)
 	}
 	return (
 		<main className="noteFound">
