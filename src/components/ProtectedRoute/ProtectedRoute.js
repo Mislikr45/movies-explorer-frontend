76eb47735c5ab0085c7594ef15bfcc23a7d477bb
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-09-30 18:04:47 +0500

    2

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -5,7 +5,9 @@ import { Navigate } from "react-router-dom";
 const ProtectedRouteElement = ({ element: Component, ...props }) => {
   return props.loggedIn ? (
     <Component {...props} />
-  ) 
+  ) : (
+    <Navigate to="/" replace />
+  );
 };
 
 export default ProtectedRouteElement;
