6267b8c9d38e1e73ace454231cda75863f140740
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-10-01 14:02:49 +0500

    1

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -22,6 +22,7 @@ export default function Search({
 
   const handleSubmit = (event) => {
     getMovieFunc()
+    console.log(movieFuncDone)
     if(movieFuncDone) {
       console.log()
       event.preventDefault();
